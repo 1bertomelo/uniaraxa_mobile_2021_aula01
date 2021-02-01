1bf6eb092c0ecbbc77a006acda99354f8a7b3435
1bertomelo <1bertomelo@gmail.com>
2021-02-01 13:43:46 -0300

    primeira aula

---
+++ src/index.js
@@ -0,0 +1,41 @@
+const express = require('express');
+const app = express();
+
+app.use(express.json());
+
+app.get('/', (request, response) => {
+    return response.json({ 'message': 'Olá Mundo' });
+});
+
+app.get('/students', (request, response) => {
+    const { name } = request.query;
+
+    return response.json({ 'message': `Olá Mundo ${name}!` });
+});
+
+app.post('/students', (request, response) => {
+    const { name, email } = request.body;
+    return response.json({ name, email });
+
+});
+
+
+app.put('/students/:id', (request, response) => {
+    const { id } = request.params;
+    const { name, email } = request.body;
+    return response.json({ name, email });
+
+});
+
+
+app.delete('/students/:id', (request, response) => {
+    const { id } = request.params;
+
+    return response.json({ message: `O id ${id} foi removido com sucesso` });
+
+});
+
+
+app.listen(3333, () => {
+    console.log("Server running...");
+});
